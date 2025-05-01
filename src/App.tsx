00b123136a307425ff5a611d95ac5a49d59b2fd8
OTonGitHub <purple@daraknet.cc>
2025-05-01 14:00:35 +0500

    pass data & callback as props

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import Message from './components/Message';
 import ListGroup from './components/ListGroup';
 import './App.css';
 
+const regions =
+  Math.random() < 0.3
+    ? [
+        "Male'",
+        "Hulhumale' Phase 1",
+        "Hulhumale' Phase 2",
+        "Villi Male'",
+        'Addu',
+      ]
+    : [];
+
+const handleSetRegion = (item: string) => {
+  console.log(item);
+};
+
 function App() {
   return (
     <div>
       <Message />
-      <ListGroup />
+      <ListGroup items={regions} heading='Region' onSelect={handleSetRegion} />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,51 @@
-function ListGroup() {
+import { MouseEvent, useState } from 'react';
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelect: (item: string) => void;
+}
+
+function ListGroup({ heading, items, onSelect }: Readonly<Props>) {
+  // avoiding dot to allow SonarScan
+  const [selectIX, setSelectIX] = useState(-1);
+
+  const handleClick = (event: MouseEvent, area: string, index: number) => {
+    setSelectIX(index);
+    console.log(
+      `Clicked ${index + 1} (${event.pageX}, ${event.pageY}): ${area}`
+    );
+  };
+
+  const generateLG = (list: string[]) => {
+    if (list.length === 0) return <p>N/A</p>;
+    return (
+      <ul className='list-group'>
+        {list.map((item, index) => (
+          <button
+            key={item}
+            className={
+              index === selectIX
+                ? 'list-group-item list-group-item-action active'
+                : 'list-group-item list-group-item-action'
+            }
+            onClick={(e) => {
+              handleClick(e, item, index); // refactor to use set, aka remove handler
+              onSelect(item);
+            }}
+          >
+            {item}
+          </button>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <ul className='list-group'>
-      <li className='list-group-item list-group-item-action'>Item-1</li>
-      <li className='list-group-item list-group-item-action'>Item-2</li>
-      <li className='list-group-item list-group-item-action'>Item-3</li>
-      <li className='list-group-item list-group-item-action'>Item-4</li>
-      <li className='list-group-item list-group-item-action'>Item-5</li>
-    </ul>
+    <>
+      <h2>{heading}</h2>
+      {generateLG(items)}
+    </>
   );
 }
 
